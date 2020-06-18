5bf610ec25d215ee2a009e8c58400d545ea3d691
Theo Jolliffe <theojolliffe@gmail.com>
2020-06-18 17:04:14 +0100

    Froze my app

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import csv
 from flask import Flask
-from flask import render_template
 from flask import abort
+from flask import render_template
 app = Flask(__name__)
 
 def get_csv():
     csv_path = './static/la-riots-deaths.csv'
-    csv_file = open(csv_path, 'rb')
+    csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
     return csv_list
@@ -27,5 +27,4 @@ def detail(row_id):
     abort(404)
 
 if __name__ == '__main__':
-    # Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
