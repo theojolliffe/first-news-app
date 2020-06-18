77a605f0c1dedf113b93f1b646f1ec6c316a3716
Theo Jolliffe <theojolliffe@gmail.com>
2020-06-18 15:48:54 +0100

    Created detail page about each vivtim

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ def index():
     object_list = get_csv()
     return render_template(template, object_list=object_list)
 
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+
 if __name__ == '__main__':
     # Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
